2f7f6c990a4eafc6862eb0581d9a9f9dcd7d58cc
Jac22 <jacopo.prudentino@gmail.com>
2023-04-26 14:31:19 +0200

    Aggiunti e completati i test

---
+++ tester/Calculator.tester.cs
@@ -0,0 +1,42 @@
+using net_calculator_tester;
+namespace tester
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+
+        [Test(Description = "Prova Add")]
+        [TestCase(15.9f, 18.98f)]
+        public void AddTest(float a, float b)
+        {
+            Assert.That(Calculator.Add(a, b), Is.EqualTo(a + b));
+        }
+
+        [Test(Description = "Prova Subtract")]
+        [TestCase(15.9f, 18.98f)]
+        public void SubtractTest(float a, float b)
+        {
+            Assert.That(Calculator.Subtract(a, b), Is.EqualTo(a - b));
+        }
+
+        [Test(Description = "Prova Multiply")]
+        [TestCase(15.9f, 18.98f)]
+        public void MultiplyTest(float a, float b)
+        {
+            Assert.That(Calculator.Multiply(a, b), Is.EqualTo(a * b));
+        }
+
+        [Test(Description = "Prova Divide")]
+        [TestCase(15.9f, 18.98f)]
+        [TestCase(15.9f, 0f)]
+        public void DivideTest(float a, float b)
+        {
+            Assume.That(b != 0);
+            Assert.That(Calculator.Divide(a, b), Is.EqualTo(a / b));
+        }
+    }
+}
