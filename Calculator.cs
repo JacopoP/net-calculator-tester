9fe7b9800b50401218299cf5cc983598931f8666
Jac22 <jacopo.prudentino@gmail.com>
2023-04-26 14:28:22 +0200

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_calculator_tester
+{
+    public static class Calculator
+    {
+        public static float Add(float a, float b) {  return a + b; }
+        public static float Subtract(float a, float b) { return a - b; }
+        public static float Multiply(float a, float b) { return a * b; }
+        public static float Divide(float a, float b) 
+        {
+            if (b == 0)
+                return float.NaN;
+            return a / b; 
+        }
+    }
+}
